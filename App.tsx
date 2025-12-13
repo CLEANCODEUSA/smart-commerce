b4c6462e173cd4428ce134510724f623566fbee7
CLEANCODE <help@simplecssjs.com>
2025-12-12 21:04:19 -0500

    101. AppButton

--- App.tsx
+++ App.tsx
@@ -3,13 +3,25 @@ import { StyleSheet, Text, View } from 'react-native';
 import AppText from './src/components/texts/AppText';
 import AppSaveView from './src/components/views/AppSaveView';
 import FlashMessage, { showMessage } from 'react-native-flash-message';
+import AppButton from './src/components/AppButton';
+import { AppColors } from './src/styles/colors';
 
 export default function App() {
   return (
     <>
       <AppSaveView style={styles.container}>
         <FlashMessage position={"top"} />
-        <AppText variant='medium'>Hello World!</AppText>
+        <AppButton title={"Show Message"} 
+          onPress={() =>{
+            showMessage({
+              message: "Hello World ^_^!",
+              color: "white",
+              type: "danger"
+            })
+          }}
+          backgroundColor='red'
+          textColor={AppColors.white}
+          />
         <AppText
           onPress={() =>{
             showMessage({
---
+++ src/components/buttons/AppButton.tsx
@@ -0,0 +1,67 @@
+import {
+  StyleSheet,
+  TouchableOpacity,
+  ViewStyle,
+  TextStyle,
+  GestureResponderEvent,
+} from 'react-native'
+import React from 'react'
+import { s, vs } from 'react-native-size-matters'
+import { AppColors } from '../../styles/colors'
+import AppText from '../texts/AppText'
+
+interface AppButtonProps {
+  title: string
+  onPress: (event: GestureResponderEvent) => void
+  backgroundColor?: string
+  textColor?: string
+  style?: ViewStyle | ViewStyle[]
+  styleTitle?: TextStyle | TextStyle[]
+  disabled?: boolean
+}
+
+const AppButton: React.FC<AppButtonProps> = ({
+  onPress,
+  title,
+  backgroundColor = AppColors.blueGray,
+  textColor = AppColors.white,
+  style,
+  styleTitle,
+  disabled = false,
+}) => {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      activeOpacity={0.8}
+      style={[
+        styles.button,
+        { backgroundColor: disabled ? AppColors.disabledGray : backgroundColor },
+        style,
+      ]}
+      disabled={disabled}
+    >
+      <AppText
+        variant="bold"
+        style={[styles.textTitle, { color: textColor }, styleTitle]}
+      >
+        {title}
+      </AppText>
+    </TouchableOpacity>
+  )
+}
+
+export default AppButton
+
+const styles = StyleSheet.create({
+  button: {
+    width: '100%',
+    height: vs(40),
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: s(25),
+    alignSelf: 'center',
+  },
+  textTitle: {
+    fontSize: s(16),
+  },
+})
