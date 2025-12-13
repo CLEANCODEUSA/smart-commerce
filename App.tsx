567d54fa634ef35a1ee845224553f2c3df1b8506
CLEANCODE <help@simplecssjs.com>
2025-12-12 20:27:59 -0500

    100. Add flash message

--- App.tsx
+++ App.tsx
@@ -3,24 +3,23 @@ import { StyleSheet, Text, View } from 'react-native';
 import AppText from './src/components/texts/AppText';
 import AppSaveView from './src/components/views/AppSaveView';
 import FlashMessage, { showMessage } from 'react-native-flash-message';
-import AppButton from './src/components/AppButton';
 
 export default function App() {
   return (
     <>
-    <FlashMessage position={"top"}/>
-    <AppSaveView style={styles.container}>
-      <AppButton title={"Show Message"} />
-      <AppText 
-        onPress={() => {
-          showMessage({
-            message: "Hello World ^_^!",
-            color: "yellow",
-            type:"danger"
-          })
-        }}
-      variant='bold'>Hello World!</AppText>
-    </AppSaveView>
+      <AppSaveView style={styles.container}>
+        <FlashMessage position={"top"} />
+        <AppText variant='medium'>Hello World!</AppText>
+        <AppText
+          onPress={() =>{
+            showMessage({
+              message: "Hello World ^_^!",
+              color: "white",
+              type: "danger"
+            })
+          }}
+            variant='bold'>Hello World!</AppText>
+      </AppSaveView>
     </>
   );
 }
