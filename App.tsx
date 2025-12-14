43e668d5f3ccaa35e6d612e436fca9c39685774b
CLEANCODE <help@simplecssjs.com>
2025-12-14 18:42:50 -0500

    104. Sign Up  Screen

--- App.tsx
+++ App.tsx
@@ -4,12 +4,14 @@ import AppSaveView from './src/components/views/AppSaveView';
 import FlashMessage, { showMessage } from 'react-native-flash-message';
 import AppTextInput from './src/components/inputs/AppTextInput';
 import SignInScreen from './src/screens/auth/SignInScreen';
+import SignUpScreen from './src/screens/auth/SignUpScreen';
 
 export default function App() {
   return (
     <>
       <FlashMessage position={"top"} />
-      <SignInScreen />
+      {/* <SignInScreen /> */}
+      <SignUpScreen />
     </>
   );
 }
