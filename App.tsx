c37108e383ea2592d8194bf9ed85fb55d86e6d6c
CLEANCODE <help@simplecssjs.com>
2025-12-14 16:42:30 -0500

    102. AppTextInput

--- App.tsx
+++ App.tsx
@@ -1,36 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import AppText from './src/components/texts/AppText';
 import AppSaveView from './src/components/views/AppSaveView';
 import FlashMessage, { showMessage } from 'react-native-flash-message';
-import AppButton from './src/components/AppButton';
-import { AppColors } from './src/styles/colors';
+import AppTextInput from './src/components/inputs/AppTextInput';
 
 export default function App() {
   return (
     <>
+      <FlashMessage position={"top"} />
       <AppSaveView style={styles.container}>
-        <FlashMessage position={"top"} />
-        <AppButton title={"Show Message"} 
-          onPress={() =>{
-            showMessage({
-              message: "Hello World ^_^!",
-              color: "white",
-              type: "danger"
-            })
-          }}
-          backgroundColor='red'
-          textColor={AppColors.white}
-          />
-        <AppText
-          onPress={() =>{
-            showMessage({
-              message: "Hello World ^_^!",
-              color: "white",
-              type: "danger"
-            })
-          }}
-            variant='bold'>Hello World!</AppText>
+        <AppTextInput 
+          placeholder='Enter Name'
+        />
       </AppSaveView>
     </>
   );
--- src/components/AppButton.tsx
+++ src/components/AppButton.tsx
@@ -1,24 +1,51 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import {
+  StyleSheet,
+  TouchableOpacity,
+  ViewStyle,
+  TextStyle,
+  GestureResponderEvent,
+} from 'react-native'
 import React from 'react'
 import { s, vs } from 'react-native-size-matters'
-import AppText from './texts/AppText'
-import { AppColors } from '../styles/colors'
+import { AppColors } from '../../styles/colors'
+import AppText from '../texts/AppText'
 
-const AppButton = ({onPress, title, backgroundColor = AppColors.primary, textColor = AppColors.black}) => {
+interface AppButtonProps {
+  title: string
+  onPress: (event: GestureResponderEvent) => void
+  backgroundColor?: string
+  textColor?: string
+  style?: ViewStyle | ViewStyle[]
+  styleTitle?: TextStyle | TextStyle[]
+  disabled?: boolean
+}
+
+const AppButton: React.FC<AppButtonProps> = ({
+  onPress,
+  title,
+  backgroundColor = AppColors.blueGray,
+  textColor = AppColors.white,
+  style,
+  styleTitle,
+  disabled = false,
+}) => {
   return (
     <TouchableOpacity
       onPress={onPress}
       activeOpacity={0.8}
-      style={
-        [
-          styles.button,
-          {
-            backgroundColor: backgroundColor
-          }
-        ]
-      }
+      style={[
+        styles.button,
+        { backgroundColor: disabled ? AppColors.disabledGray : backgroundColor },
+        style,
+      ]}
+      disabled={disabled}
     >
-      <AppText style={[styles.textTitle, {color: textColor}]} variant='bold'>{title}</AppText>
+      <AppText
+        variant="bold"
+        style={[styles.textTitle, { color: textColor }, styleTitle]}
+      >
+        {title}
+      </AppText>
     </TouchableOpacity>
   )
 }
@@ -26,15 +53,15 @@ const AppButton = ({onPress, title, backgroundColor = AppColors.primary, textCol
 export default AppButton
 
 const styles = StyleSheet.create({
-  button:{
-    width: "100%",
+  button: {
+    width: '100%',
     height: vs(40),
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     borderRadius: s(25),
-    alignSelf: "center"
+    alignSelf: 'center',
   },
-  textTitle:{
-    fontSize: s(16)
-  }
+  textTitle: {
+    fontSize: s(16),
+  },
 })
