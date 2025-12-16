4b044e379085850a741f1e5cb31acc9b28000c87
CLEANCODE <help@simplecssjs.com>
2025-12-16 15:39:37 -0500

    114. Props

--- src/components/cards/ProductCard.tsx
+++ src/components/cards/ProductCard.tsx
@@ -1,30 +1,37 @@
 import { StyleSheet, Text, Image, View, TouchableOpacity } from 'react-native'
-import React from 'react'
-import { AppColors } from '../../styles/colors'
+import React, { FC } from 'react'
 import { s, vs } from "react-native-size-matters"
 import AppText from '../texts/AppText'
+import { AppColors } from '../../styles/colors'
 import { AppFonts } from '../../styles/fonts'
 import { Ionicons } from "@expo/vector-icons"
 import { commonStyles } from '../../styles/sharedStyles'
 
-const ProductCard = () => {
+interface IProductCard {
+  onPress: () => void;
+  title: string;
+  price: string;
+  imageURL: string;
+}
+
+const ProductCard: FC<IProductCard> = ({ onPress, imageURL, title, price }) => {
   return (
     <View style={styles.container}>
 
       {/* Add to Cart Button */}
-      <TouchableOpacity style={styles.addToCartButton}>
+      <TouchableOpacity style={styles.addToCartButton} onPress={onPress}>
         <Ionicons name='cart' size={s(15)} color={AppColors.white} />
       </TouchableOpacity>
 
       {/* Image UI section */}
       <View style={styles.imageContainer}>
-        <Image style={styles.image} source={{ uri: "https://images.pexels.com/photos/12794494/pexels-photo-12794494.jpeg" }} />
+        <Image style={styles.image} source={{ uri: imageURL }} />
       </View>
 
       {/* Details */}
       <View style={styles.detailsContainer}>
-        <AppText style={styles.titleText}>iPhone 15</AppText>
-        <AppText style={styles.priceText}>$1200.00 USD</AppText>
+        <AppText style={styles.titleText}>{title}</AppText>
+        <AppText style={styles.priceText}>{price}</AppText>
       </View>
     </View>
   )
