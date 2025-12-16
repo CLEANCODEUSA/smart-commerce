bd13baa0be09edac7804580eef62724941436819
CLEANCODE <help@simplecssjs.com>
2025-12-16 11:27:42 -0500

    108. App Header

--- src/screens/profile/ProfileScreen.tsx
+++ src/screens/profile/ProfileScreen.tsx
@@ -1,11 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import HomeHeader from '../../components/headers/HomeHeader'
+import AppSaveView from '../../components/views/AppSaveView'
 
 const ProfileScreen = () => {
   return (
-    <View>
+    <AppSaveView>
+      <HomeHeader />
       <Text>ProfileScreen</Text>
-    </View>
+    </AppSaveView>
   )
 }
 
